31f5020a7ee82e9e369cb130b7de1f25caa27765
usmanasif-sandhu <usmanasifsandhu.682@gmail.com>
2026-08-16 12:37:46 +0500

    specs updated

--- assets/assets.js
+++ assets/assets.js
@@ -21,7 +21,6 @@ export const categories = [
 
 export const ourSpecsData = [
     { title: "Free Shipping", description: "Enjoy fast, free delivery on every order no conditions, just reliable doorstep.", icon: SendIcon, accent: '#05DF72' },
-    { title: "7 Days easy Return", description: "Change your mind? No worries. Return any item within 7 days.", icon: ClockFadingIcon, accent: '#FF8904' },
     { title: "24/7 Customer Support", description: "We're here for you. Get expert help with our customer support.", icon: HeadsetIcon, accent: '#A684FF' }
 ]
 
